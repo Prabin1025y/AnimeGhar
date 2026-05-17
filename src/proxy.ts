fd26b4a1522a6097c52129e21b3e6964a845584c
Prabin1025y <acharyaprabin1025y@gmail.com>
2026-05-17 12:44:10 +0545

    refactor: uncomment rate limiting logic in proxy middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,28 +4,28 @@ import { ratelimit } from "./lib/ratelimit";
 
 // This function can be marked `async` if using `await` inside
 export default async function proxy(request: NextRequest) {
-    // const forwarded = request.headers.get("x-forwarded-for");
-    // const realIp = request.headers.get("x-real-ip");
+    const forwarded = request.headers.get("x-forwarded-for");
+    const realIp = request.headers.get("x-real-ip");
 
-    // const ip = forwarded?.split(",")[0]?.trim() || realIp || "127.0.0.1";
+    const ip = forwarded?.split(",")[0]?.trim() || realIp || "127.0.0.1";
 
-    // const { success, limit, remaining, reset } = await ratelimit.limit(ip);
+    const { success, limit, remaining, reset } = await ratelimit.limit(ip);
 
-    // if (!success) {
-    //     return NextResponse.json(
-    //         {
-    //             message: "Too many requests!",
-    //         },
-    //         {
-    //             status: 429,
-    //             headers: {
-    //                 "X-Ratelimit-Limit": limit.toString(),
-    //                 "X-Ratelimit-Remaining": remaining.toString(),
-    //                 "X-Ratelimit-Reset": reset.toString(),
-    //             },
-    //         },
-    //     );
-    // }
+    if (!success) {
+        return NextResponse.json(
+            {
+                message: "Too many requests!",
+            },
+            {
+                status: 429,
+                headers: {
+                    "X-Ratelimit-Limit": limit.toString(),
+                    "X-Ratelimit-Remaining": remaining.toString(),
+                    "X-Ratelimit-Reset": reset.toString(),
+                },
+            },
+        );
+    }
 
     return NextResponse.next()
 }
